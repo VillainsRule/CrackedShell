f26cb6e1efa63c31c011461f3483fa54fc5b30e7
thonk <hello@villainsrule.xyz>
2025-01-12 09:21:14 -0500

    use static bun routes & ping endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
 import Bun from 'bun';
+import path from 'node:path';
 
-import server from './console/server';
 import game from './game/rewrite';
 import files from './game/files';
 
 import config from '../config';
 
+const build = async (file: string) => {
+    let data = Bun.file(path.join(import.meta.dirname, file));
+
+    return new Response(await data.bytes(), {
+        headers: {
+            'Content-Type': data.type
+        }
+    })
+}
+
 Bun.serve({
     port: config.port,
+
+    static: {
+        '/$': await build('./console/index.html'),
+        '/$/': await build('./console/index.html'),
+
+        '/$/style.css': await build('./console/style.css'),
+        '/$/script.js': await build('./console/script.js'),
+
+        '/$/ping': new Response('OK')
+    },
 
     fetch: async (request) => {
         const url = new URL(request.url);
         const path = decodeURIComponent(url.pathname);
 
-        if (path.startsWith('/$')) return server({ path });
+        if (path.startsWith('/$')) return new Response();
         else if (path.startsWith('/?') || path === '/') return await game({ url });
         else return await files({ path });
     }
